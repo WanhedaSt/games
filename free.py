81d2fa20e4f4e12dd7fce3f2bcdb4b3b220317a6
WanhedaSt <storewanheda@gmail.com>
2024-06-04 19:43:52 -0300

    Create free.py

---
+++ free.py
@@ -0,0 +1,4 @@
+games = {
+    "1": ("Macro", "https://drive.usercontent.google.com/download?id=1rTmdaAsOXoVDV38tKOoBPR3-iYWpHFSH&export=download&authuser=0&confirm=t&uuid=7152d8c1-3263-4ebe-98e8-f02bec080f30&at=APZUnTUAXAnQm47X8hqwOuYSX9Dx%3A1717540975114", "online"),
+    "2": ("Valorant Trigger", "https://drive.usercontent.google.com/download?id=1nxWybIcVNBRU1qayYVo7ht9d7Q-G-TrC&export=download&authuser=0", "online"),
+}
