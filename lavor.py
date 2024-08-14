015bc73b5ac673ceeeb83960334c8d9a6ae94d4e
WanhedaSt <storewanheda@gmail.com>
2024-08-14 00:06:59 -0300

    Create lavor.py

---
+++ lavor.py
@@ -0,0 +1,6 @@
+games = {
+    "1": ("Valorant Legit X", "https://drive.usercontent.google.com/download?id=1gxGSHa6YibG4EzfSaWLohRxKKiyPVAPW&export=download&authuser=0&confirm=t&uuid=00c4f25a-9a9a-49bb-98e3-055b85f885de&at=APZUnTVha_ySwECyhY3ftX6jG1oI%3A1723483512288", "online"),
+    "2": ("Valorant Legit V1", "https://drive.usercontent.google.com/download?id=1DX8UssYGJTZ7yfcj-HtBMuq8ByQUPBps&export=download&authuser=0&confirm=t&uuid=24fc83a0-fc9c-4476-8f4d-b77025c6c89a&at=APZUnTWh3qXSSdUn6vfRr3-ly02N%3A1723483544944", "online"),
+    "3": ("Valorant Legit V2", "https://drive.usercontent.google.com/download?id=1wPyLEwhm1Kyn79PbaiogzZncYIgfUpJo&export=download&authuser=0&confirm=t&uuid=ae795ae5-d005-4f0d-a840-7c89b8edbcfa&at=APZUnTXlFN5sAoh6bIwnzaFbJmUn%3A1723483578421", "online"),
+    "4": ("Valorant Rage", "https://drive.usercontent.google.com/download?id=1-NRt_05EJHSZ_7mZiJqE1rda8TwFjbK-&export=download&authuser=0&confirm=t&uuid=a83343ab-dfac-49aa-bf63-48c2131601d9&at=APZUnTXDCmJ2nDn6ZOvC42GX53t0%3A1723481321790", "online"),
+}
