b2528fb76534a6cb89e2118630c95a84cfc80e01
WanhedaSt <storewanheda@gmail.com>
2024-09-09 14:14:14 -0300

    Update games.py

--- games.py
+++ games.py
@@ -1,7 +1,7 @@
 games = {
     "1": ("Valorant Menu", "https://drive.usercontent.google.com/download?id=1gxGSHa6YibG4EzfSaWLohRxKKiyPVAPW&export=download&authuser=0&confirm=t&uuid=00c4f25a-9a9a-49bb-98e3-055b85f885de&at=APZUnTVha_ySwECyhY3ftX6jG1oI%3A1723483512288", "online"),
     "2": ("Valorant Legit", "https://drive.usercontent.google.com/download?id=1DX8UssYGJTZ7yfcj-HtBMuq8ByQUPBps&export=download&authuser=0&confirm=t&uuid=24fc83a0-fc9c-4476-8f4d-b77025c6c89a&at=APZUnTWh3qXSSdUn6vfRr3-ly02N%3A1723483544944", "online"),
-    "3": ("Valorant ESP+AIM", "https://drive.usercontent.google.com/download?id=1-NRt_05EJHSZ_7mZiJqE1rda8TwFjbK-&export=download&authuser=0&confirm=t&uuid=a83343ab-dfac-49aa-bf63-48c2131601d9&at=APZUnTXDCmJ2nDn6ZOvC42GX53t0%3A1723481321790", "online"),
+    "3": ("Valorant ESP+AIM", "https://drive.usercontent.google.com/download?id=1-NRt_05EJHSZ_7mZiJqE1rda8TwFjbK-&export=download&authuser=0&confirm=t&uuid=a83343ab-dfac-49aa-bf63-48c2131601d9&at=APZUnTXDCmJ2nDn6ZOvC42GX53t0%3A1723481321790", "offline"),
     "4": ("Pubg Legit", "https://drive.usercontent.google.com/download?id=1RuSBUbUtLdmqAG5m90tYYaT6ctqlUCW8&export=download&authuser=0&confirm=t&uuid=77d13a3c-46af-47e7-9d5b-23f3c932ac92&at=AO7h07f-2BBBNfrgdXF16svjylEQ%3A1724374343761", "online"),
     "5": ("Squad Legit", "https://drive.usercontent.google.com/download?id=1A8WrdgRLgxOv9ltjyFJDqTZAFy1WaqRF&export=download&authuser=0&confirm=t&uuid=e063ba86-3095-497c-8414-6f569432217d&at=AO7h07dWJtMgJLVNoWVByk4HvYh1%3A1724374365015", "online"),
     "6": ("DayZ Legit", "https://drive.usercontent.google.com/download?id=1x_WOlKeTuNRlZKzqGCrEPJDALHUUEm1i&export=download&authuser=0&confirm=t&uuid=1103d68c-4389-4d79-bc4d-9bd5d809e3e5&at=AO7h07ePg_J7wqcFUUGl50DjDVUL%3A1724374384535", "online"),
