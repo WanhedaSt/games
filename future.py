ca03d30a9d03ba3292238b51d4b28203f8a5e63c
WanhedaSt <storewanheda@gmail.com>
2024-09-15 21:22:35 -0300

    Create future.py

---
+++ future.py
@@ -0,0 +1,4 @@
+games = {
+    "1": ("Valorant Aimb0t", "https://drive.usercontent.google.com/download?id=1DX8UssYGJTZ7yfcj-HtBMuq8ByQUPBps&export=download&authuser=0&confirm=t&uuid=24fc83a0-fc9c-4476-8f4d-b77025c6c89a&at=APZUnTWh3qXSSdUn6vfRr3-ly02N%3A1723483544944", "online"),
+    "2": ("Spoofer + TPM", "https://drive.usercontent.google.com/download?id=1wxCobK0DEZfzGMhyCdQEgLq2bEEHPBlV&export=download&authuser=0&confirm=t&uuid=803ffbcc-eed5-4c71-bf47-e3012b511b42&at=AO7h07fo5WG6h6blIQbzUHndy6S6%3A1725941655641", "online"),
+}
