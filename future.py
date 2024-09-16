6b19e263e89e1c9bdd2f8f2f6f2be23598ab1723
WanhedaSt <storewanheda@gmail.com>
2024-09-16 00:44:02 -0300

    Update future.py

--- future.py
+++ future.py
@@ -1,4 +1,5 @@
 games = {
     "1": ("Valorant Aimb0t", "https://drive.usercontent.google.com/download?id=1DX8UssYGJTZ7yfcj-HtBMuq8ByQUPBps&export=download&authuser=0&confirm=t&uuid=24fc83a0-fc9c-4476-8f4d-b77025c6c89a&at=APZUnTWh3qXSSdUn6vfRr3-ly02N%3A1723483544944", "online"),
-    "2": ("Spoofer + TPM", "https://drive.usercontent.google.com/download?id=1wxCobK0DEZfzGMhyCdQEgLq2bEEHPBlV&export=download&authuser=0&confirm=t&uuid=803ffbcc-eed5-4c71-bf47-e3012b511b42&at=AO7h07fo5WG6h6blIQbzUHndy6S6%3A1725941655641", "online"),
+    "2": ("Valorant Aimb0t ( Menu )", "https://drive.usercontent.google.com/download?id=1gxGSHa6YibG4EzfSaWLohRxKKiyPVAPW&export=download&authuser=0&confirm=t&uuid=00c4f25a-9a9a-49bb-98e3-055b85f885de&at=APZUnTVha_ySwECyhY3ftX6jG1oI%3A1723483512288", "online"),
+    "3": ("Spoofer + TPM", "https://drive.usercontent.google.com/download?id=1wxCobK0DEZfzGMhyCdQEgLq2bEEHPBlV&export=download&authuser=0&confirm=t&uuid=803ffbcc-eed5-4c71-bf47-e3012b511b42&at=AO7h07fo5WG6h6blIQbzUHndy6S6%3A1725941655641", "online"),
 }
