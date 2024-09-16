45f386c4cf9bc443aaa93c5ac3f103ca9cfb5afb
WanhedaSt <storewanheda@gmail.com>
2024-09-16 19:44:17 -0300

    Update future.py

--- future.py
+++ future.py
@@ -1,5 +1,7 @@
 games = {
     "1": ("Valorant Aimb0t", "https://drive.usercontent.google.com/download?id=1DX8UssYGJTZ7yfcj-HtBMuq8ByQUPBps&export=download&authuser=0&confirm=t&uuid=24fc83a0-fc9c-4476-8f4d-b77025c6c89a&at=APZUnTWh3qXSSdUn6vfRr3-ly02N%3A1723483544944", "online"),
-    "2": ("Valorant Aimb0t ( Menu )", "https://drive.usercontent.google.com/download?id=1gxGSHa6YibG4EzfSaWLohRxKKiyPVAPW&export=download&authuser=0&confirm=t&uuid=00c4f25a-9a9a-49bb-98e3-055b85f885de&at=APZUnTVha_ySwECyhY3ftX6jG1oI%3A1723483512288", "online"),
-    "3": ("Spoofer + TPM", "https://drive.usercontent.google.com/download?id=1wxCobK0DEZfzGMhyCdQEgLq2bEEHPBlV&export=download&authuser=0&confirm=t&uuid=803ffbcc-eed5-4c71-bf47-e3012b511b42&at=AO7h07fo5WG6h6blIQbzUHndy6S6%3A1725941655641", "online"),
+    "2": ("Valorant Aimb0t ( Menu )", "https://drive.usercontent.google.com/download?id=1gxGSHa6YibG4EzfSaWLohRxKKiyPVAPW&export=download&authuser=0&confirm=t&uuid=00c4f25a-9a9a-49bb-98e3-055b85f885de&at=APZUnTVha_ySwECyhY3ftX6jG1oI%3A1723483512288", "online
+    "3": ("Spectre Divide - Aim", "https://drive.usercontent.google.com/download?id=1V7QI57J5LugjVznlopJpRh1u4JYAvI6V&export=download&authuser=0&confirm=t&uuid=5bac8e5d-e0e1-43f2-a97c-30ee8bb0e470&at=AO7h07f92cWH6DJTsLPlTXQ6WDB3%3A1726521823848", "online"),
+    "4": ("Spectre Divide - Esp+Aim", "https://drive.usercontent.google.com/download?id=1rcdrMDg22l7_zW5cVhFzL5cTt8M5B9G-&export=download&authuser=0&confirm=t&uuid=95668537-fb74-4857-8819-00776955c2bb&at=AO7h07d818149jQIBEVhIQrh1bhu%3A1726521858610", "online"),
+    "5": ("Spoofer + TPM", "https://drive.usercontent.google.com/download?id=1wxCobK0DEZfzGMhyCdQEgLq2bEEHPBlV&export=download&authuser=0&confirm=t&uuid=c977b756-b0f5-4c0f-8c09-dc23b2970b8f&at=AO7h07fHWCz-MIDlyPoh8-1pAub2%3A1726523429569", "online"),
 }
