03e04a5b73eaa27ff411b241f87285696490f3a1
WanhedaSt <storewanheda@gmail.com>
2024-09-20 00:59:19 -0300

    Update and rename future.py to rp.py

---
+++ rp.py
@@ -0,0 +1,4 @@
+games = {
+    "1": ("Valorant - Aim", "https://drive.usercontent.google.com/download?id=1DX8UssYGJTZ7yfcj-HtBMuq8ByQUPBps&export=download&authuser=0&confirm=t&uuid=24fc83a0-fc9c-4476-8f4d-b77025c6c89a&at=APZUnTWh3qXSSdUn6vfRr3-ly02N%3A1723483544944", "online"),
+    "2": ("Pubg - External", "https://drive.usercontent.google.com/download?id=1RuSBUbUtLdmqAG5m90tYYaT6ctqlUCW8&export=download&authuser=0&confirm=t&uuid=77d13a3c-46af-47e7-9d5b-23f3c932ac92&at=AO7h07f-2BBBNfrgdXF16svjylEQ%3A1724374343761", "offline"),
+}
