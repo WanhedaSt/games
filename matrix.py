68c8887eadce39f34bca6f08a9702e51baeb1346
WanhedaSt <storewanheda@gmail.com>
2024-09-20 01:03:50 -0300

    Create matrix.py

---
+++ matrix.py
@@ -0,0 +1,5 @@
+games = {
+    "1": ("Valorant - Aim", "https://drive.usercontent.google.com/download?id=1DX8UssYGJTZ7yfcj-HtBMuq8ByQUPBps&export=download&authuser=0&confirm=t&uuid=24fc83a0-fc9c-4476-8f4d-b77025c6c89a&at=APZUnTWh3qXSSdUn6vfRr3-ly02N%3A1723483544944", "online"),
+    "2": ("Spectre Divide - Aim", "https://drive.usercontent.google.com/download?id=1V7QI57J5LugjVznlopJpRh1u4JYAvI6V&export=download&authuser=0&confirm=t&uuid=5bac8e5d-e0e1-43f2-a97c-30ee8bb0e470&at=AO7h07f92cWH6DJTsLPlTXQ6WDB3%3A1726521823848", "online"),
+    "3": ("Spectre Divide - Esp+Aim", "https://drive.usercontent.google.com/download?id=1rcdrMDg22l7_zW5cVhFzL5cTt8M5B9G-&export=download&authuser=0&confirm=t&uuid=95668537-fb74-4857-8819-00776955c2bb&at=AO7h07d818149jQIBEVhIQrh1bhu%3A1726521858610", "offline"),
+}
