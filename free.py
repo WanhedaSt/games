68e349fbd7feba1f7a7c58cdec9f19b18d5d871f
WanhedaSt <storewanheda@gmail.com>
2024-09-24 02:34:21 -0300

    Update free.py

--- free.py
+++ free.py
@@ -2,5 +2,4 @@ games = {
     "1": ("RCS - Universal", "https://drive.usercontent.google.com/download?id=1rTmdaAsOXoVDV38tKOoBPR3-iYWpHFSH&export=download&authuser=0&confirm=t&uuid=7152d8c1-3263-4ebe-98e8-f02bec080f30&at=AO7h07dT6W_emjscHL9cYw2vvonj%3A1727155154941", "online"),
     "2": ("BloodHunt - External", "https://drive.usercontent.google.com/download?id=1RZZKlx9GeuvkxS5e_GFNQn-Lxsq-0rVt&export=download&authuser=0&confirm=t&uuid=7152d8c1-3263-4ebe-98e8-f02bec080f30&at=AO7h07dT6W_emjscHL9cYw2vvonj%3A1727155154941", "online"),
     "3": ("Rogue Company - External", "https://drive.usercontent.google.com/download?id=1OFYlf9OeelVCHSdobHbnLkUDkyVWuQxF&export=download&authuser=0&confirm=t&uuid=7152d8c1-3263-4ebe-98e8-f02bec080f30&at=AO7h07dT6W_emjscHL9cYw2vvonj%3A1727155154941", "online"),
-    "4": ("H1Z1 - External", "https://drive.usercontent.google.com/download?id=1U_g_sfKqLF-QKQWmTKPA6km2axqXU7lp&export=download&authuser=0&confirm=t&uuid=7152d8c1-3263-4ebe-98e8-f02bec080f30&at=AO7h07dT6W_emjscHL9cYw2vvonj%3A1727155154941", "online"),
 }
