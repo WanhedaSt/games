4964e95043a4f9c1949ad1cb26fd014385fc25f6
WanhedaSt <storewanheda@gmail.com>
2024-11-23 19:05:07 -0300

    Update rp.py

--- rp.py
+++ rp.py
@@ -1,4 +1,8 @@
 games = {
-    "1": ("Valorant - Aim", "https://drive.usercontent.google.com/download?id=1DX8UssYGJTZ7yfcj-HtBMuq8ByQUPBps&export=download&authuser=0&confirm=t&uuid=24fc83a0-fc9c-4476-8f4d-b77025c6c89a&at=APZUnTWh3qXSSdUn6vfRr3-ly02N%3A1723483544944", "online"),
-    "2": ("Pubg - External", "https://drive.usercontent.google.com/download?id=1RuSBUbUtLdmqAG5m90tYYaT6ctqlUCW8&export=download&authuser=0&confirm=t&uuid=77d13a3c-46af-47e7-9d5b-23f3c932ac92&at=AO7h07f-2BBBNfrgdXF16svjylEQ%3A1724374343761", "offline"),
+    "1": ("Valorant - Aim", "https://drive.usercontent.google.com/download?id=1DX8UssYGJTZ7yfcj-HtBMuq8ByQUPBps&export=download&authuser=0&confirm=t&uuid=e3a99e11-73ec-4334-b7c1-26b88a82e676&at=AO7h07f7bKnqeNndMqpvyCBIRSYH%3A1727139311235", "online", "1.0.1"),
+    "2": ("Pubg - External", "https://drive.usercontent.google.com/download?id=1RuSBUbUtLdmqAG5m90tYYaT6ctqlUCW8&export=download&authuser=0&confirm=t&uuid=77d13a3c-46af-47e7-9d5b-23f3c932ac92&at=AO7h07f-2BBBNfrgdXF16svjylEQ%3A1724374343761", "online", "1.0.1"),
+    "3": ("DayZ - External", "https://drive.usercontent.google.com/download?id=1x_WOlKeTuNRlZKzqGCrEPJDALHUUEm1i&export=download&authuser=0&confirm=t&uuid=1103d68c-4389-4d79-bc4d-9bd5d809e3e5&at=AO7h07ePg_J7wqcFUUGl50DjDVUL%3A1724374384535", "online", "1.0.1"),
+    "4": ("FiveM - External", "https://drive.usercontent.google.com/download?id=1OKO_OPoe9L3YVQenUT53Ut7MlAcF-7Bh&export=download&authuser=0&confirm=t&uuid=d56fc74a-bfa9-476e-87b8-520922918afb&at=APZUnTWWRhHLTjN8GAQnnkE-pd2h%3A1723483652664", "online", "1.0.1"),
+    "5": ("Valorant - PrivAim", "https://drive.usercontent.google.com/download?id=1D1Tp93IG-cyPBzkeDaxDh4oVKGmCvVBU&export=download&authuser=0&confirm=t&uuid=e3a99e11-73ec-4334-b7c1-26b88a82e676&at=AO7h07f7bKnqeNndMqpvyCBIRSYH%3A1727139311235", "online", "1.0.1"),
+    "6": ("Valorant - PrivEsp", "https://drive.usercontent.google.com/download?id=1fAI5T2yadeKpCe_rPnf2KFkm2jYNpq1q&export=download&authuser=0&confirm=t&uuid=e3a99e11-73ec-4334-b7c1-26b88a82e676&at=AO7h07f7bKnqeNndMqpvyCBIRSYH%3A1727139311235", "online", "1.0.1"),
 }
