689e0d1bd84a029aed1cf4748192ff66b02ab2ff
WanhedaSt <storewanheda@gmail.com>
2024-12-01 01:42:49 -0300

    Update free.py

--- free.py
+++ free.py
@@ -1,5 +1,3 @@
 games = {
-    "1": ("RCS - Universal", "https://drive.usercontent.google.com/download?id=1rTmdaAsOXoVDV38tKOoBPR3-iYWpHFSH&export=download&authuser=0&confirm=t&uuid=7152d8c1-3263-4ebe-98e8-f02bec080f30&at=AO7h07dT6W_emjscHL9cYw2vvonj%3A1727155154941", "online"),
-    "2": ("BloodHunt - External", "https://drive.usercontent.google.com/download?id=1RZZKlx9GeuvkxS5e_GFNQn-Lxsq-0rVt&export=download&authuser=0&confirm=t&uuid=7152d8c1-3263-4ebe-98e8-f02bec080f30&at=AO7h07dT6W_emjscHL9cYw2vvonj%3A1727155154941", "online"),
-    "3": ("Rogue Company - External", "https://drive.usercontent.google.com/download?id=1OFYlf9OeelVCHSdobHbnLkUDkyVWuQxF&export=download&authuser=0&confirm=t&uuid=7152d8c1-3263-4ebe-98e8-f02bec080f30&at=AO7h07dT6W_emjscHL9cYw2vvonj%3A1727155154941", "online"),
+    "1": ("Contact the creator for reactivation.", "", "offline")
 }
