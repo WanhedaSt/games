3f4ea596c6673d0e8465f1c361713674d204b216
WanhedaSt <storewanheda@gmail.com>
2024-12-12 00:05:01 -0300

    Update and rename config.json to config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+config = {
+    "loader_title": "GAMING STORE | v2.0.0",
+    "loader_subtitle": "VIP VERSION",
+    "zip_password": "123",
+    # Adicione outras configurações aqui conforme necessário
+}
